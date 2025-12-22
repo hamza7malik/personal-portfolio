943527affd92c6bcc6270e9aba51ae1bf3fa18dc
hamza7malik <crhamza7@gmail.com>
2025-12-22 14:20:36 +0400

    update chat styles

--- components/Chatbot/Chatbot.tsx
+++ components/Chatbot/Chatbot.tsx
@@ -186,7 +186,12 @@ const Chatbot: React.FC<ChatbotProps> = ({isOpen, onClose}) => {
                     viewBox="0 0 24 24"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="12" cy="12" r="11" fill="var(--accent-color)" />
+                    <circle
+                      cx="12"
+                      cy="12"
+                      r="11"
+                      fill="var(--primary-color)"
+                    />
                     <path
                       d="M12 12C13.6569 12 15 10.6569 15 9C15 7.34315 13.6569 6 12 6C10.3431 6 9 7.34315 9 9C9 10.6569 10.3431 12 12 12Z"
                       fill="white"
